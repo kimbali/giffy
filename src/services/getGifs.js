ab9a2ca9db0ca2aea8404d1b0b4a9e722df8140f
Kimbali <kimgarcianton@hotmail.com>
2021-03-07 20:27:59 +0100

    feat: :rocket: Add more gifs on the call

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -13,7 +13,7 @@ const fromApiResponseToGifs = apiResponse => {
     return []
 }
 
-export default function getGifs ({ limit = 5, keyword = 'Ricky', page = 0} ) {
+export default function getGifs ({ limit = 15, keyword = 'Ricky', page = 0} ) {
     // Paginacion, offset = cuantos resultados tienes que saltarte
     const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en`
     return fetch(apiURL)
